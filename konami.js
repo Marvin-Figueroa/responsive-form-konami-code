fc8b8eb133a2becd9df77851ad26266c493157ef
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-10-31 14:25:43 -0600

    feat(signup form): add konami code functionality

---
+++ konami.js
@@ -0,0 +1,17 @@
+const pressedKeys = [];
+const konamiCode = "magic";
+const main = document.querySelector(".signup");
+
+window.addEventListener("keydown", (e) => {
+  pressedKeys.push(e.key.toLowerCase());
+
+  if (pressedKeys.length > konamiCode.length) {
+    pressedKeys.shift();
+  }
+
+  if (pressedKeys.join("") === konamiCode) {
+    main.classList.add("konami");
+  } else {
+    main.classList.remove("konami");
+  }
+});
