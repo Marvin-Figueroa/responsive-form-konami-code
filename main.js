4798e8ca5bd0975c3ee8ccc47ddfb71029445ad3
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-10-31 12:34:42 -0600

    feat(signup form): add form validation logic

---
+++ main.js
@@ -0,0 +1,81 @@
+const form = document.querySelector(".signup__form");
+const formInputs = document.querySelectorAll(".form__input");
+const submitBtn = document.querySelector(".form__input--submit");
+
+const simpleWebsiteRegEx = /^(https:|http:|www\.)\S*/gm;
+const simplePhoneRegEx = /^[\d]{4}-[\d]{4}$/;
+let formIsValid;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  formIsValid = true;
+
+  formInputs.forEach((input) => {
+    removeErrorClasses(input.parentElement);
+
+    if (input.value.trim().length === 0) {
+      input.parentElement.classList.add("input-error", "error-empty");
+      formIsValid = false;
+    } else if (input.getAttribute("name").includes("name")) {
+      if (!/^[a-zA-Z]+$/.test(input.value.trim())) {
+        input.parentElement.classList.add("input-error", "error-format-text");
+        formIsValid = false;
+      }
+    } else if (input.getAttribute("name").includes("password")) {
+      if (form.password.value.trim() !== form.passwordConfirm.value.trim()) {
+        input.parentElement.classList.add(
+          "input-error",
+          "error-password-mismatch"
+        );
+        formIsValid = false;
+      }
+    } else if (input.getAttribute("name") === "phone") {
+      if (!simplePhoneRegEx.test(input.value.trim())) {
+        input.parentElement.classList.add("input-error", "error-format-phone");
+        formIsValid = false;
+      }
+    } else if (input.getAttribute("name") === "website") {
+      if (!simpleWebsiteRegEx.test(input.value.trim())) {
+        input.parentElement.classList.add(
+          "input-error",
+          "error-format-website"
+        );
+        formIsValid = false;
+      }
+    } else if (input.getAttribute("name") === "age") {
+      if (!/^[0-9]+$/.test(input.value.trim())) {
+        input.parentElement.classList.add("input-error", "error-format-number");
+        formIsValid = false;
+      }
+    }
+  });
+
+  if (formIsValid) {
+    printFormValues();
+    e.target.reset();
+  }
+});
+
+function removeErrorClasses(inputElement) {
+  inputElement.classList.remove(
+    "input-error",
+    "error-empty",
+    "error-format-website",
+    "error-format-text",
+    "error-password-mismatch",
+    "error-format-phone",
+    "error-format-number"
+  );
+}
+
+function printFormValues() {
+  formInputs.forEach((input) =>
+    console.log(`${input.getAttribute("name")}: ${input.value}`)
+  );
+  console.log(
+    `${form.experience.getAttribute("name")}: ${form.experience.value}`
+  );
+  console.log(
+    `${form.newsletter.getAttribute("name")}: ${form.newsletter.checked}`
+  );
+}
